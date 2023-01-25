6b004b9c0b8ac25e8d42a1f96b02e5afc0dc92ec
Omar Alvarez <oalvareze11@gmail.com>
2023-01-25 16:58:04 -0600

    tipos de datos.

---
+++ 01-typeScript-intro/src/bases/01-types.ts
@@ -0,0 +1,11 @@
+export const name = "Omar Alvarez";
+
+export const templateString = `
+Esto es un string multilinea
+que puede contener espacios, 
+" dobles
+' simples
+imprimir cosas como la suma de ${1+1}
+`
+
+console.log(templateString);
--- 01-typeScript-intro/src/main.ts
+++ 01-typeScript-intro/src/main.ts
@@ -1,8 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { setupCounter } from "./counter";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+import { name } from "./bases/01-types";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -11,6 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
+    <h2>Hello ${name}</h2>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
@@ -18,6 +21,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
